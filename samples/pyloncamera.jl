042f159ee45c50080c1c881d2d254cad54a342fc
Jesper Stemann Andersen <jsa@ihp.dk>
2019-03-14 17:23:35 +0100

    PylonCamera: take! returns a PooledArray
    * Added support for BGR/RGB images
    * Added control of max_num_buffer
    * Added separate open!/close! methods

--- samples/pyloncamera.jl
+++ samples/pyloncamera.jl
@@ -1,20 +1,27 @@
-using Pkg
-Pkg.activate(".")
-
 include(joinpath("..", "src", "PylonCameras.jl"))
-using Cameras
 
-const images_to_grab = 10
+using Cameras
+using ResourcePools
+
+const images_to_grab = 30
+const max_num_buffer = 8
 const grab_result_wait_timeout_ms = 100
 const grab_result_retrieve_timeout_ms = 1
 
-camera = PylonCameras.PylonCamera(grab_result_wait_timeout_ms, grab_result_retrieve_timeout_ms)
-println("Using $(PylonCameras.info(camera))")
-start!(camera, images_to_grab)
+camera = PylonCameras.PylonCamera(max_num_buffer, grab_result_wait_timeout_ms, grab_result_retrieve_timeout_ms)
+try
+    println("Using $(PylonCameras.info(camera))")
+    start!(camera, images_to_grab)
 
-while isrunning(camera)
-    grab_result, img = take!(camera)
-    print("$(size(img)) : ")
-    @show img[1,1]
-    PylonCameras.release!(grab_result)
+    while isrunning(camera)
+        img = take!(camera)
+        print("$(size(img)) : ")
+        @show img[1,1,1]
+        release!(img)
+    end
+catch e
+    println(e)
+finally
+    stop!(camera)
+    PylonCameras.close!(camera)
 end
--- src/PylonCameras.jl
+++ src/PylonCameras.jl
@@ -1,6 +1,7 @@
 module PylonCameras
 
 using Cameras
+using ResourcePools
 
 import Cameras:
     isrunning,
@@ -11,6 +12,8 @@ import Cameras:
 
 export PylonCamera,
     isrunning,
+    open!,
+    close!,
     start!,
     stop!,
     take!,
@@ -24,13 +27,14 @@ mutable struct PylonCamera <: Camera
     grab_result_ready_cond::Base.AsyncCondition
     grab_result_wait_timeout_ms::UInt32
     grab_result_retrieve_timeout_ms::UInt32
-    function PylonCamera(grab_result_wait_timeout_ms::Int, grab_result_retrieve_timeout_ms::Int)
+    function PylonCamera(max_num_buffer::Int, grab_result_wait_timeout_ms::Int, grab_result_retrieve_timeout_ms::Int)
         Wrapper.pylon_initialize()
         transport_layer_factory = Wrapper.get_transport_layer_factory_instance()
         grab_result_ready_cond = Base.AsyncCondition()
 
         device = Wrapper.create_first_device(transport_layer_factory)
         instant_camera = Wrapper.InstantCamera(device)
+        Wrapper.max_num_buffer!(instant_camera, UInt(max_num_buffer))
         new(device, instant_camera, grab_result_ready_cond, grab_result_wait_timeout_ms, grab_result_retrieve_timeout_ms)
     end
 end
@@ -44,24 +48,42 @@ function info(c::PylonCamera)
 end
 
 isrunning(c::PylonCamera) = Wrapper.is_grabbing(c.instant_camera)
+open!(c::PylonCamera) = Wrapper.open(c.instant_camera)
+close!(c::PylonCamera) = Wrapper.close(c.instant_camera)
 start!(c::PylonCamera) = Wrapper.start_grabbing_async(c.instant_camera, c.grab_result_wait_timeout_ms, Wrapper.notify_async_cond_safe_c, c.grab_result_ready_cond.handle)
 start!(c::PylonCamera, images_to_grab::Int) = Wrapper.start_grabbing_async(c.instant_camera, UInt64(images_to_grab), c.grab_result_wait_timeout_ms, Wrapper.notify_async_cond_safe_c, c.grab_result_ready_cond.handle)
 stop!(c::PylonCamera) = Wrapper.stop_grabbing(c.instant_camera)
 
 function take!(c::PylonCamera)
+    @debug "Waiting for result"
     wait(c.grab_result_ready_cond)
+    @debug "Retrieving result for $(c.grab_result_retrieve_timeout_ms) ms"
+    # Be careful not to pass grab_result on (e.g. to active logging like @info),
+    # as it may cause the grab_result to be held for too long exhausting the pylon camera's buffer pool
     grab_result = Wrapper.retrieve_result(c.instant_camera, c.grab_result_retrieve_timeout_ms)
+    @debug "Retrieved $(grab_result)"
     if Wrapper.grab_succeeded(grab_result)
         width = Wrapper.get_width(grab_result)
         height = Wrapper.get_height(grab_result)
         buffer = Wrapper.get_buffer(grab_result)
-        buffer_array = unsafe_wrap(Array, Ptr{UInt8}(buffer), (width, height))
-        return (grab_result, buffer_array)
+        pixel_type = Wrapper.get_pixel_type(grab_result)
+        if Wrapper.is_bgr(pixel_type) || Wrapper.is_rgb(pixel_type)
+            samples_per_pixel = Wrapper.samples_per_pixel(pixel_type)
+            size = (samples_per_pixel, width, height)
+        else
+            size = (width, height)
+        end
+        @assert prod(size) == Wrapper.get_image_size(grab_result)
+        buffer_array = unsafe_wrap(Array, Ptr{UInt8}(buffer), size)
+        function dispose(a)
+            @debug "Releasing $(grab_result)"
+            Wrapper.release(grab_result)
+        end
+        return PooledArray(buffer_array, dispose)
     else
         error("$(Wrapper.get_error_code(grab_result)) $(Wrapper.get_error_description(grab_result))")
     end
 end
-release!(grab_result) = Wrapper.release(grab_result)
 
 trigger!(camera::PylonCamera) = error("Not implemented yet")
 
