ed69d32dd058503e7d4c5623e507d06dcf668676
Jesper Stemann Andersen <jsa@ihp.dk>
2019-03-21 11:27:37 +0100

    Simplified usage in sample

--- samples/pyloncamera.jl
+++ samples/pyloncamera.jl
@@ -1,14 +1,14 @@
 include(joinpath("..", "src", "PylonCameras.jl"))
 
 using Cameras
-using ResourcePools
+using .PylonCameras
 
 const images_to_grab = 30
 const max_num_buffer = 8
 const grab_result_wait_timeout_ms = 100
 const grab_result_retrieve_timeout_ms = 1
 
-camera = PylonCameras.PylonCamera(max_num_buffer, grab_result_wait_timeout_ms, grab_result_retrieve_timeout_ms)
+camera = PylonCamera(max_num_buffer, grab_result_wait_timeout_ms, grab_result_retrieve_timeout_ms)
 try
     println("Using $(PylonCameras.info(camera))")
     start!(camera, images_to_grab)
@@ -23,5 +23,5 @@ catch e
     println(e)
 finally
     stop!(camera)
-    PylonCameras.close!(camera)
+    close!(camera)
 end
