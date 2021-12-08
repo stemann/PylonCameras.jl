4e5360e894589a3d54c729c218565ee3d3699e82
Jesper Stemann Andersen <jsa@ihp.dk>
2021-12-08 16:40:04 +0100

    Added tests for constructing PylonCamera with vendor name, model name or serial number

--- test/pylon_camera_tests.jl
+++ test/pylon_camera_tests.jl
@@ -2,19 +2,51 @@ using PylonCameras
 using Test
 
 @testset "pylon_camera_tests" begin
-    ENV["PYLON_CAMEMU"] = 1
+    ENV["PYLON_CAMEMU"] = 2
 
-    camera = PylonCamera()
+    expected_vendor_name = "Basler"
+    expected_model_name = "Emulation"
+    expected_serial_number(n::Int) = "0815-$(lpad(string(n), 4, '0'))"
 
-    @testset "info" begin
-        expected_vendor_name = "Basler"
-        expected_model_name = "Emulation"
-        expected_serial_number(n::Int) = "0815-$(lpad(string(n), 4, '0'))"
+    @testset "Construction" begin
+        camera = PylonCamera()
 
         vendor_name, model_name, serial_number = PylonCameras.info(camera)
 
         @test vendor_name == expected_vendor_name
         @test model_name == expected_model_name
         @test serial_number == expected_serial_number(0)
+    end
+
+    @testset "Construction with vendor_name" begin
+        camera = PylonCamera(vendor_name = expected_vendor_name)
+
+        vendor_name, model_name, serial_number = PylonCameras.info(camera)
+
+        @test vendor_name == expected_vendor_name
+        @test model_name == expected_model_name
+        @test serial_number == expected_serial_number(0)
+    end
+
+    @testset "Construction with model_name" begin
+        camera = PylonCamera(model_name = expected_model_name)
+
+        vendor_name, model_name, serial_number = PylonCameras.info(camera)
+
+        @test vendor_name == expected_vendor_name
+        @test model_name == expected_model_name
+        @test serial_number == expected_serial_number(0)
+    end
+
+    @testset "Construction with serial_number" begin
+        selected_serial_number = expected_serial_number(1)
+
+        camera = PylonCamera(serial_number = selected_serial_number)
+
+        vendor_name, model_name, serial_number = PylonCameras.info(camera)
+
+        @test vendor_name == expected_vendor_name
+        @test model_name == expected_model_name
+        @test serial_number == selected_serial_number
     end
 end
